bf88d6c95f9e1d55dcfccff6bfb33be0d8388f93
MohammedAliIbrahim <mhalieg@yahoo.com>
2021-03-30 20:56:37 +0200

    Add project files.

---
+++ WindowsFormsSaveaAndShowData/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsSaveaAndShowData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            //comboBox1.Items.Add(2010);
+            //comboBox1.Items.Add(2011);
+            //comboBox1.Items.Add(2012);
+            //comboBox1.Items.Add(2013);
+            //comboBox1.Items.Add(2014);
+
+            List<object> yearsList = new List<object>() { 2010, 2011, 2012, 2013, 2014 };
+
+          
+            comboBox1.Items.AddRange(yearsList.ToArray()) ;
+
+
+
+
+        }
+    }
+}
